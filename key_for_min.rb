4d04830f671c44db128a3aefa26a468abc999ea8
Aian Soktoev <soktoevayan@gmail.com>
2020-06-01 23:02:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,11 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
   name_hash.each do |k, v|
     compare_num = 1.0/0 
-    
+    if v < compare_num 
+      compare_num = v 
+    end
+  end 
+  puts k 
 end
+
+key_for_min_value(hash)
